0456bb6e38149b837a79d53b1276fb3f42acef93
oliverlogie <oliver.s.kraus@gmail.com>
2020-02-14 17:30:23 +0100

    done with to-do part

--- js/myscript.js
+++ js/myscript.js
@@ -1,14 +1,32 @@
 var movies = data;
 
+var likeCounter = [];
+
+movies.forEach( function(){
+    likeCounter.push(Math.floor(Math.random()* Math.floor(10)));
+    }
+)
+
+
+// functions
+const addLike = function(index) {
+    // likeCounter[index] += 1;
+    var like = $(`#likes${index}`).html();
+    like++;
+    $(`#likes${index}`).html(like);
+}
+
+// functions end
+
 for (var i = 0; i < movies.length; i++) {
-    var card =
-    `<div class="col-lg-5 media bg-dark text-white p-4 mb-3">
+	var card =
+	`<div id="container" class="col-lg-5 media bg-dark text-white p-4 mb-3">
     <img class="media-left pr-4" alt="Image" src="${movies[i].image}">
     <div class="media-body">
         <h4 class="card-title">${movies[i].name}</h4>
         <p class="card-text">${movies[i].desc}</p>
-        <div class="alignButton"><button type="button" class="btn bg-dark text-success btn-sm">Like <i class="fas fa-thumbs-up"></i> </button>
-            <span class="likes">8</span>
+        <div class="alignButton"><button onclick="addLike(${i})" id="likeButton" type="button" class="btn bg-dark text-success btn-sm">Like <i class="fas fa-thumbs-up"></i> </button>
+            <span id="likes${i}" class="likes b-0">${likeCounter[i]}</span>
         </div>
     </div>
 </div>`;
